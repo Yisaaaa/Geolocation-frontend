e7fd62527fef5a799efbd8e72718c1d2032ad8ba
Yisaaaa <ryansanisit19@gmail.com>
2025-12-09 20:33:43 +0800

    Update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import { useAuthStore } from "@/store/authStore";
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api";
 
 export const api = axios.create({
   baseURL: `${API_BASE_URL}`,
