25934d2e379b4076fdaf5ecf86e9a91bb9039eda
Yisaaaa <ryansanisit19@gmail.com>
2025-12-09 20:24:31 +0800

    Update

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -29,6 +29,8 @@ export default function HomePage() {
     fetchHistory();
   }, []);
 
+  console.log(isHistoryLoading);
+
   const fetchCurrentLocation = async () => {
     console.log("fetching current location");
     try {
